2b19cb47f416d31d6c655a443d656206c442de64
--help <taipham@cisco.com>
2018-04-01 12:01:10 -0700

    fix minor stuff

--- src/main/java/com/example/grpc/PingClient.java
+++ src/main/java/com/example/grpc/PingClient.java
@@ -10,17 +10,18 @@ public class PingClient
     public static void main( String[] args ) throws Exception
     {
       // Set locolhost port 8080
-      final ManagedChannel channel = ManagedChannelBuilder.forTarget("169.254.19.43:8080")
+      final ManagedChannel channel = ManagedChannelBuilder.forTarget("169.254.90.246:3000")
         .usePlaintext(true)
         .build();
 
       CommunicationServiceGrpc.CommunicationServiceBlockingStub stub = CommunicationServiceGrpc.newBlockingStub(channel);
 
-      CommunicationServiceOuterClass.Ping ping =
+      CommunicationServiceOuterClass.Ping request =
       CommunicationServiceOuterClass.Ping.newBuilder()
           .setRequest(true)
           .build();
 
+      /*
       CommunicationServiceOuterClass.Header request =
       CommunicationServiceOuterClass.Header.newBuilder()
           .setFromIp("169.254.99.162")
@@ -30,13 +31,13 @@ public class PingClient
           .setPing(ping)
           .setToken("1234567890")
           .build();
-
+          */
 
       // Finally, make the call using the stub
       //CommunicationServiceOuterClass.TransferDataResponse response =
       //  stub.communication(request);
-      CommunicationServiceOuterClass.Header response =
-        stub.messageHandler(request);
+      CommunicationServiceOuterClass.Ping response =
+        stub.pingHandler(request);
 
       System.out.println(response);
 
