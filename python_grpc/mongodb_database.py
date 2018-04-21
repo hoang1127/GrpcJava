5505826767f6a1f3c7522d7bb7bae8a5cbf3f037
--help <taipham@cisco.com>
2018-04-20 19:22:31 -0700

    update db code

--- python_grpc/mongodb_database.py
+++ python_grpc/mongodb_database.py
@@ -6,6 +6,23 @@ import subprocess
 import time
 import datetime
 
+'''
+Mesowest header
+'STN YYMMDD/HHMM MNET SLAT SLON SELV TMPF SKNT DRCT GUST PMSL ALTI DWPF RELH WTHR P24I'
+
+Mesonet header
+'# id,name,mesonet,lat,lon,elevation,agl,cit,state,country,active'
+'''
+
+CONST_MESONET_DELIMITER = ','
+
+CONST_DELIMITER = ','
+CONST_NEWLINE_CHAR = '\n'
+
+# station, timestamp_utc, mnet??, latitude, longitude, temperature, ...
+CONST_STD_COL_LIST = CONST_MESOWEST_HEADER.split()
+CONST_MESONET_COL_LIST = ['id','name','mesonet','lat','lon','elevation','agl','cit','state','country','active']
+
 def insert_bulk_mongo(db, data):
     for d in data:
         print d
@@ -38,72 +55,50 @@ def insert_bulk_mongo(db, data):
         print 'One post: {0}'.format(result.inserted_id)
 
 def insert_bulk_mongo_mesonet(db, data, timestamp):
-  try:
-    #create dynamic query and insert into DB
-    #TODO need to have a better solution for ID --> primary key
-    #id = 0
-    #letter = string.ascii_uppercase
-    #variable = ['id']
+    '''
+    Mesowest header
+    'STN YYMMDD/HHMM MNET SLAT SLON SELV TMPF SKNT DRCT GUST PMSL ALTI DWPF RELH WTHR P24I'
+
+    Mesonet header
+    '# id,name,mesonet,lat,lon,elevation,agl,cit,state,country,active'
+
+    Matching:
+    # id (0) -> STN
+    lat (3) -> SLAT
+    long (4) -> SLON
+    elevation (5) -> SELV
+    '''
     for d in data:
-      id += 1
-      values = [id]
-      if len(d) == 0:
-        continue
-      try:
-        variable.extend(list(map(lambda x: x[0], d)))
-        values.extend(list(map(lambda x: float(x[1]) if str(x[1])[0] not in letter and x[0]!='TIMESTAMP' else
-                      (str('\''+str(x[1])+'\'') if x[0]!='TIMESTAMP' else int(x[1])), d ) ))
-        mesowestTuple = {
-          "id":values[0],
-          "STID":values[1],
-          "STNM":values[2],
-          "timestamp":values[3],
-          "RELH":values[4],
-          "TAIR":values[5],
-          "WSPD":values[6],
-          "WVEC":values[7],
-          "WDIR":values[8],
-          "WDSD":values[9],
-          "WSSD":values[10],
-          "WMAX":values[11],
-          "RAIN":values[12],
-          "PRES":values[13],
-          "SRAD":values[14],
-          "TA9M":values[15],
-          "WS2M":values[16],
-          "TS10":values[17],
-          "TB10":values[18],
-          "TS05":values[19],
-          "TS25":values[20],
-          "TS60":values[21],
-          "TR05":values[22],
-          "TR25":values[23],
-          "TR60":values[24]
-        }
-        #TODO dynamic query assembling needed
-        #session.execute(mesowest_insert_query, tuple(values))
-        db.mesowest.insert_one(mesowestTuple)
-      except Exception as e:
-        logging.warning(e)
-        continue
-    print('finished loading all data')
-  except Exception as e:
-    logging.error(e)
-
-
-def find_mongo(db, startDate, endDate):
-    value = []
-    try:
+        print d
+        values = d.split(',')
+        # Convert time from 20180316_2145 to 20180316/2145
+        timestamp.replace('_', '/')
+        #dateTimeObject = datetime.datetime.strptime(values[1], '%Y%m%d/%H%M')
+        #print dateTimeObject
         pattern = '%Y%m%d/%H%M'
-        startDateTS = int(time.mktime(time.strptime(startDate, pattern))) * 1000
-        endDateTS = int(time.mktime(time.strptime(endDate, pattern))) * 1000
-        result=db.mesowest.find({"timestamp":{"$lt": endDateTS, "$gte": startDateTS}})
-        for document in result:
-            print(document)
-            value.append(document)
-        return value
-    except Exception as e:
-        logging.warning(e)
+        time_t = int(time.mktime(time.strptime(timestamp, pattern))) * 1000
+        print time_t
+        input_data = {
+            "STN":values[0],
+            "timestamp":time_t,
+            "MNET": "NULL",
+            "SLAT":values[3],
+            "SLON":values[4],
+            "SELV":values[5],
+            "TMPF":"NULL",
+            "SKNT":"NULL",
+            "DRCT":"NULL",
+            "GUST":"NULL",
+            "PMSL":"NULL",
+            "ALTI":"NULL",
+            "DWPF":"NULL",
+            "RELH":"NULL",
+            "WTHR":"NULL",
+            "P24I":"NULL"
+        }
+        #print input_data
+        result = db.mesowest.insert_one(input_data)
+        print 'One post: {0}'.format(result.inserted_id)
 
 def test():
     client = MongoClient('localhost', 27017)
