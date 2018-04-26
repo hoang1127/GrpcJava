5bf0dd1b004877c49a8f2af7ffafb86d9b00bf97
hoang1127 <hoang1127@gmail.com>
2018-04-26 11:03:02 -0700

    Clean up

--- ProjectCluster/client.sh
+++ ProjectCluster/client.sh
@@ -6,8 +6,8 @@ echo "RunClient from ${SVR_HOME}"
 
 echo server home = $SVR_HOME
 
-JAVA_APP = 'gash.router.app.ClientApp'
-JAVA_ARG = "localhost 5123"
-JAVA_SER = '-client -Djava.net.preferIPv4Stack=true'
+JAVA_APP='gash.router.app.ClientApp'
+JAVA_ARG="localhost 5123"
+JAVA_SER='-client -Djava.net.preferIPv4Stack=true'
 
 java ${JAVA_SER} -cp .:${SVR_HOME}/lib/'*':${SVR_HOME}/classes ${JAVA_APP} $1
